98d155b92c13d3c19e08f55e2db552317869ce65
Arthur_Chen <nammount19@gmail.com>
2026-01-25 15:12:08 +0700

    setup cors include những domain nào có thể truy cập được vào domain backend của mình

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import post, user,auth,vote
@@ -6,6 +8,15 @@ from .routers import post, user,auth,vote
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ['https://www.google.com']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Những phương thức được phép
+    allow_headers=["*"], 
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
